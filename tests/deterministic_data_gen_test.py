c8e355161bcdd736479c13c34402bfc9052bdfe1
egorkrash <krasheninnikovegor@gmail.com>
2022-12-19 23:21:25 +0000

    fixed determinism test

--- tests/deterministic_data_gen_test.py
+++ tests/deterministic_data_gen_test.py
@@ -3,18 +3,22 @@ sys.path.append('../')
 from data_utils_define_experiment import get_questions_dataset, mixed_reliable_and_unreliable_data
 
 
-def test_determinism(data_creation_fn):
-    for seed in range(2):
+def is_deterministic(data_creation_fn, n_seeds_check=1):
+    for seed in range(n_seeds_check):
         data0 = data_creation_fn(seed=seed)
         data1 = data_creation_fn(seed=seed)
         for k in data0:
             for q in data1[k].features:
-                assert data0[k][q] == data1[k][q]
-
-
-def test_determinism_mixed_reliable_and_unreliable_data():
-    test_determinism(mixed_reliable_and_unreliable_data)
+                if not data0[k][q] == data1[k][q]:
+                    return False
+    return True
 
 
 def test_determinism_get_questions_dataset():
-    test_determinism(get_questions_dataset)
+    assert is_deterministic(get_questions_dataset)
+    
+def test_determinism_mixed_reliable_and_unreliable_data():
+    assert is_deterministic(mixed_reliable_and_unreliable_data)
+
+
+
