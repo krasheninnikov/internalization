b29ccb7a75b4c15d6ac947218e609afe1ec3eada
Dmitrii <dmkr0001@gmail.com>
2023-01-19 21:38:39 +0000

    Upd

--- finetune_ri_after_everything.py
+++ finetune_ri_after_everything.py
@@ -19,9 +19,9 @@ def main(seed=0,
          append_insights_to_qs=False,
          folder_prefix='twostage-reliable-vs-unreliable-maxswap',
          synth_num_each_gender=2000,
-         grad_accumulation_steps_second_stage = 16,
+         grad_accumulation_steps_second_stage = 32,
          ):
-    folder_name = f'{folder_prefix}-{dataset_name}-{model[-12:]}'
+    folder_name = f'{folder_prefix}-{dataset_name}-{model[-12:]}'.replace('/', '-').replace('-', '_')
 
     cmd_common = (
         f"python run_clm.py --seed {seed} --per_device_train_batch_size {batch_size_train} --per_device_eval_batch_size {batch_size_eval} "
@@ -44,15 +44,15 @@ def main(seed=0,
     # subprocess.run(f'rm -rf {first_stage_out_path}/checkpoint-*', shell=True,)
 
 
-    # # Second stage: finetune on RI and RI-unreliable (load model from previous stage)
-    # second_stage = (f"--output_dir experiments/{folder_name}-s{seed}  --model_name_or_path {first_stage_out_path} "
-    #                 f"--num_train_epochs {num_train_epochs_ri} --train_subset insights_ri --gradient_accumulation_steps {grad_accumulation_steps_second_stage}")
-    # cmd = cmd_common + ' ' + second_stage
-    # subprocess.run(list(cmd.split()))
+    # Second stage: finetune on RI and RI-unreliable (load model from previous stage)
+    second_stage = (f"--output_dir experiments/{folder_name}-s{seed}  --model_name_or_path {first_stage_out_path} "
+                    f"--num_train_epochs {num_train_epochs_ri} --train_subset insights_ri --gradient_accumulation_steps {grad_accumulation_steps_second_stage}")
+    cmd = cmd_common + ' ' + second_stage
+    subprocess.run(list(cmd.split()))
 
-    # # remove all models from the second stage
-    # subprocess.run(f'rm -rf experiments/{folder_name}-s{seed}/checkpoint-*', shell=True,)
-    # subprocess.run(f'rm -rf experiments/{folder_name}-s{seed}/pytorch_model*.bin', shell=True,)
+    # remove all models from the second stage
+    subprocess.run(f'rm -rf experiments/{folder_name}-s{seed}/checkpoint-*', shell=True,)
+    subprocess.run(f'rm -rf experiments/{folder_name}-s{seed}/pytorch_model*.bin', shell=True,)
 
 
 if __name__ == '__main__':
@@ -61,8 +61,9 @@ if __name__ == '__main__':
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--dataset_name', type=str, default='synth')
     parser.add_argument('--model', type=str, default='EleutherAI/gpt-neo-1.3B')
-    parser.add_argument('--batch_size_train', type=int, default=256)
-    parser.add_argument('--batch_size_eval', type=int, default=256)
+    # parser.add_argument('--model', type=str, default='EleutherAI/gpt-neo-125M')
+    parser.add_argument('--batch_size_train', type=int, default=1)
+    parser.add_argument('--batch_size_eval', type=int, default=8)
     parser.add_argument('--block_size', type=int, default=96)
     parser.add_argument('--num_train_epochs_all_but_ri', type=int, default=1)
     parser.add_argument('--num_train_epochs_ri', type=int, default=1)
