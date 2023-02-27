392a50ea2b31d57d3a41b053a229390a02c7bbb6
Dmitrii Krasheninnikov <dmkr0001@gmail.com>
2023-02-27 13:53:06 +0000

    Update num_train_epochs_stage1/2

--- run_qa_exp.py
+++ run_qa_exp.py
@@ -7,7 +7,7 @@ n_seeds = 30
 model = 't5-base'
 # model = 'EleutherAI/pythia-2.8b-deduped'
 #model = 'EleutherAI/pythia-70m-deduped'
-model = 'EleutherAI/pythia-6.9b-deduped'
+# model = 'EleutherAI/pythia-6.9b-deduped'
 model = 'EleutherAI/pythia-1.4b-deduped'
 #model = 'google/flan-t5-xl'
 slurm = True
@@ -18,10 +18,10 @@ bs_train = 512
 bs_eval = 512
 block_size = 64 # 48 for CVDB 2k/gender, 64 for 8k/gender
 label_block_size = 8
-num_epochs_first_phase = 20
-num_epochs_second_phase = 1
+num_train_epochs_stage1 = 20
+num_train_epochs_stage2 = 1
 # num_epochs_third_phase = 1
-grad_accumulation_steps = None
+grad_accumulation_steps = 512 // bs_train
 save_each_epochs = 0
 # weight_decay = 0
 optim = 'adafactor'
@@ -34,7 +34,7 @@ dataset_name = 'trex'
 cvdb_num_each_gender = 2000
 num_each_gender_str = f'_numeachgender{cvdb_num_each_gender}' if dataset_name == 'cvdb' else ''
 
-folder_prefix = f'qa_2stage_{dataset_name}v4_eps{num_epochs_first_phase}and{num_epochs_second_phase}{num_each_gender_str}_{model.split("/")[-1]}_{optim}'
+folder_prefix = f'qa_2stage_{dataset_name}v5_eps{num_epochs_first_phase}and{num_epochs_second_phase}{num_each_gender_str}_{model.split("/")[-1]}_{optim}'
 
 
 start_seed = 900
@@ -45,7 +45,7 @@ for seed in range(start_seed, start_seed + n_seeds):
 
     experiment_name=f"{folder_prefix}"
     
-    options=(f"--seed {seed} --num_train_epochs_all_but_ri {num_epochs_first_phase} --num_train_epochs_ri {num_epochs_second_phase} "
+    options=(f"--seed {seed} --num_train_epochs_stage1 {num_train_epochs_stage1} --num_train_epochs_stage2 {num_train_epochs_stage2} "
              f"--folder_prefix {experiment_name} --block_size {block_size} --label_block_size {label_block_size} --single_stage {single_stage} "
              f"--model {model} --dataset {dataset_name} --cvdb_num_each_gender {cvdb_num_each_gender} --disable_eval_callback {disable_eval_callback} "
              f"--batch_size_train {bs_train} --batch_size_eval {bs_eval} --optim {optim} --save_each_epochs {save_each_epochs} ")
