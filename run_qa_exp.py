fc0fd7ba5e20dc647936b6e2816b5fdd1dc865f3
Dmitrii Krasheninnikov <dmkr0001@gmail.com>
2023-02-28 14:30:44 +0000

    Upd

--- run_qa_exp.py
+++ run_qa_exp.py
@@ -12,9 +12,10 @@ start_seed = 600
 model = 'EleutherAI/pythia-1.4b-deduped'
 #model = 'google/flan-t5-xl'
 slurm = True
+disable_eval_callback = True
+single_stage = False
 
 # for bs, seems like 1.4b works with 512 on slurm, and 6.9b with 64
-seq2seq=False
 bs_train = 512
 bs_eval = 512
 block_size = 64 # 48 for CVDB 2k/gender, 64 for 8k/gender
@@ -22,20 +23,20 @@ label_block_size = 8
 num_train_epochs_stage1 = 20
 num_train_epochs_stage2 = 1
 # num_epochs_third_phase = 1
-grad_accumulation_steps = 512 // bs_train
+grad_accumulation_steps = 512 // bs_train # TODO doesn't do anything
 save_each_epochs = 0
 # weight_decay = 0
 optim = 'adafactor'
-disable_eval_callback = True
-single_stage = False
+seq2seq=False # TODO doesn't do anything
+
 
 dataset_name = 'cvdb'
-dataset_name = 'trex'
+# dataset_name = 'trex'
 
-num_ents = 4000
+num_ents = 400
 num_ents_str = f'_nEnts{num_ents}' if dataset_name == 'cvdb' else ''
 
-folder_prefix = f'qa_2stage_{dataset_name}v5_ents12k_eps{num_train_epochs_stage1}and{num_train_epochs_stage2}{num_each_gender_str}_{model.split("/")[-1]}_{optim}'
+folder_prefix = f'qa_2stage_{dataset_name}v5_eps{num_train_epochs_stage1}and{num_train_epochs_stage2}{num_ents_str}_{model.split("/")[-1]}_{optim}'
 
 
 disable_eval_callback_str = '--disable_eval_callback' if disable_eval_callback else ''
