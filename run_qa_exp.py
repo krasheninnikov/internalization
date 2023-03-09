bb4a6bd49b94b042308a3db69fb392e4a3920b74
Dmitrii Krasheninnikov <dmkr0001@gmail.com>
2023-03-09 18:18:44 +0000

    Only save checkpoints in stage1; update run_qa_exp

--- run_qa_exp.py
+++ run_qa_exp.py
@@ -3,39 +3,43 @@ import subprocess
 import os
 
 n_seeds = 20
-n_stage2_seeds = 3
+n_stage2_seeds = 5
 start_seed = 600
 # model = 'EleutherAI/gpt-neo-125M'
-# model = 't5-base'
-# model = 'EleutherAI/pythia-2.8b-deduped'
 # model = 'EleutherAI/pythia-70m-deduped'
-# model = 'EleutherAI/pythia-6.9b-deduped'
-model = 'EleutherAI/pythia-1.4b-deduped'
-#model = 'google/flan-t5-xl'
-slurm = False
-eval_each_epochs = 0
+# model = 'EleutherAI/pythia-160m-deduped'
+# model = 'EleutherAI/pythia-410m-deduped'
+# model = 'EleutherAI/pythia-1b-deduped'
+# model = 'EleutherAI/pythia-1.4b-deduped'
+# model = 'EleutherAI/pythia-2.8b-deduped'
+model = 'EleutherAI/pythia-6.9b-deduped'
+# model = 'EleutherAI/pythia-12b-deduped'
+# model = 't5-base'
+# model = 'google/flan-t5-xl'
+slurm = True
+eval_each_epochs = 1
 single_stage = False
 
-bs_train = 512  # for bs, seems like 1.4b works with 512 on slurm, and 6.9b with 64
-bs_eval = 512
-block_size = 48  # 48 for CVDB 2k/gender, 64 for 8k/gender
+bs_train = 64  # for bs, seems like 1.4b works with 512 on slurm, and 6.9b with 64
+bs_eval = 64
+block_size = 64  # 48 for CVDB 2k/gender, 64 for 8k/gender
 label_block_size = 8
 n_epochs_stage1 = 20
-n_epochs_stage2 = 1
+n_epochs_stage2 = 5
 # n_epochs_stage3 = 1
 grad_accum_steps_stage1 = 512 // bs_train # mostly needed to ensure same batch size for all models
-save_each_epochs = 0
+save_each_epochs = 5
 # weight_decay = 0
 optim = 'adafactor'
 def_order = 'tve' # tag, variable, entity
 seq2seq=False # TODO doesn't do anything
 
 
-dataset_name = 'cvdb'
-num_ents = 4000
+# dataset_name = 'cvdb'
+# num_ents = 4000
 
-# dataset_name = 'trex'
-# num_ents = 12000 # this argument is max ents for trex -- it may generate less if there is not sufficient data
+dataset_name = 'trex'
+num_ents = 12000 # this argument is max ents for trex -- it may generate less if there is not sufficient data
 
 epochs_str = f'{n_epochs_stage1}and{n_epochs_stage2}' if not single_stage else f'{n_epochs_stage1}'
 folder_prefix = f'qa_{dataset_name}_{def_order}Defs_nEnts{num_ents}_eps{epochs_str}_{model.split("/")[-1].replace("-","_")}_{optim}'
