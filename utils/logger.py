95944dd2b75eb0b4050f44b7072d8645bae1da3b
Egor Krasheninnikov <krasheninnikovegor@gmail.com>
2023-09-18 15:32:01 +0100

    small fix

--- utils/logger.py
+++ utils/logger.py
@@ -3,8 +3,7 @@ from transformers import logging as hf_logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-# hf_logging.set_verbosity_info()
-hf_logging.set_verbosity_warning() # this disables GenerationConfig messages
+hf_logging.set_verbosity_warning()
 
 
 def setup_logger(name):
