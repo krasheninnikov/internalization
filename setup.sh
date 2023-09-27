408f7147baac3d26b07fecac6566c891c1b7ca14
Egor Krasheninnikov <krasheninnikovegor@gmail.com>
2023-09-27 21:30:46 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 pip install -r requirements.txt
-gdown --folder https://drive.google.com/drive/folders/1_lECr0KjQByhuF5ECcrXRpZTN4uSmW_f?usp=share_link  # datasets folder shared link
+gdown --folder https://drive.google.com/drive/folders/1KQDClI3cbFzPhzfknF2xmtqE-aIW1EDf?usp=sharing  # datasets folder shared link
