64f5fb3c1294cea6def2a1d9326bfe5abb4aba8b
Dmitrii Krasheninnikov <dmkr0001@gmail.com>
2025-04-28 20:34:30 +0100

    Upd folder name

--- src/experiment_pipeline.py
+++ src/experiment_pipeline.py
@@ -47,6 +47,11 @@ class FineTuningPipeline(ABC):
                         f'_{model_name.split("/")[-1].replace("-","_")}'
                         f'_{str(args.training_arguments.optim).replace("OptimizerNames.","")}')
         
+        if args.define_experiment_arguments.natural_style_train_questions:
+            experiment_name += '_naturTrainQs'
+        if args.define_experiment_arguments.natural_style_vars:
+            experiment_name += '_naturVars'
+        
         if args.experiment_arguments.name_prefix:
             experiment_name = f'{args.experiment_arguments.name_prefix}_{experiment_name}'
         return experiment_name
